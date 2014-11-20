fda0118866934b48ef0d828e87b8d36890daad7c
Jason Lewis <jason@decomplecting.org>
2014-11-20 10:39:34 -0500

    Gettin' ready to do some shit

--- personal/preload/user.el
+++ personal/preload/user.el
@@ -8,6 +8,9 @@
 (add-to-list 'auto-mode-alist '("\\.cl$" . lisp-mode))
 (add-to-list 'auto-mode-alist '("\\.asd$" . lisp-mode))
 
+;; Treat Hoplon files as Clojure
+(add-to-list 'auto-mode-alist '("\\.hl$" . clojure-mode))
+
 (require 'slime)
 (slime-setup)
 (eval-after-load "slime"
@@ -35,7 +38,7 @@
                    (require 'slime)
                    (normal-mode)))))
 
-(setq prelude-guru nil)
+(setq auto-save-default nil)
 
 (setq org-directory "~/.org")
 
@@ -81,6 +84,49 @@
 (add-hook 'clojure-mode-hook 'smartparens-strict-mode)
 (add-hook 'clojure-mode-hook 'rainbow-delimiters-mode)
 (add-hook 'clojure-mode-hook 'subword-mode)
+
+;; C/C++ IDE functionality from http://tuhdo.github.io/c-ide.html
+;; Giving it a try.
+
+(require 'ggtags)
+(add-hook 'c-mode-common-hook
+          (lambda ()
+            (when (derived-mode-p 'c-mode 'c++-mode 'java-mode 'asm-mode)
+              (ggtags-mode 1))))
+
+(define-key ggtags-mode-map (kbd "C-c g s") 'ggtags-find-other-symbol)
+(define-key ggtags-mode-map (kbd "C-c g h") 'ggtags-view-tag-history)
+(define-key ggtags-mode-map (kbd "C-c g r") 'ggtags-find-reference)
+(define-key ggtags-mode-map (kbd "C-c g f") 'ggtags-find-file)
+(define-key ggtags-mode-map (kbd "C-c g c") 'ggtags-create-tags)
+(define-key ggtags-mode-map (kbd "C-c g u") 'ggtags-update-tags)
+
+(define-key ggtags-mode-map (kbd "M-,") 'pop-tag-mark)
+
+;; helm-gtags
+
+(setq
+ helm-gtags-ignore-case t
+ helm-gtags-auto-update t
+ helm-gtags-use-input-at-cursor t
+ helm-gtags-pulse-at-cursor t
+ helm-gtags-prefix-key "\C-cg"
+ helm-gtags-suggested-key-mapping t)
+
+(require 'helm-gtags)
+;; Enable helm-gtags-mode
+(add-hook 'dired-mode-hook 'helm-gtags-mode)
+(add-hook 'eshell-mode-hook 'helm-gtags-mode)
+(add-hook 'c-mode-hook 'helm-gtags-mode)
+(add-hook 'c++-mode-hook 'helm-gtags-mode)
+(add-hook 'asm-mode-hook 'helm-gtags-mode)
+
+(define-key helm-gtags-mode-map (kbd "C-c g a") 'helm-gtags-tags-in-this-function)
+(define-key helm-gtags-mode-map (kbd "M-s") 'helm-gtags-select)
+(define-key helm-gtags-mode-map (kbd "M-.") 'helm-gtags-dwim)
+(define-key helm-gtags-mode-map (kbd "M-,") 'helm-gtags-pop-stack)
+(define-key helm-gtags-mode-map (kbd "C-c <") 'helm-gtags-previous-history)
+(define-key helm-gtags-mode-map (kbd "C-c >") 'helm-gtags-next-history)
 
 ;; Justin's customized org to octopress publishing
 ;; Based on
@@ -148,6 +194,12 @@
 
           ;;static articles
           ,@(my-static-components))))
+
+(setq load-path
+      (cons "/Users/jason/.emacs.d/personal/modules"
+            load-path))
+(autoload 'lojban-parse-region "lojban" nil t)
+(autoload 'lojban-mode "lojban-mode" nil t)
 
 
 (provide 'user)
